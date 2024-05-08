c0bb7a9111fbd07358af0c2bac5ccb294ad608c4
Gaik Artur <141954990+webDevArtur@users.noreply.github.com>
2024-05-08 14:41:10 +0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-const API_BASE_URL: string = 'https://c967-89-113-156-243.ngrok-free.app ';
+const API_BASE_URL: string = 'https://c967-89-113-156-243.ngrok-free.app';
 
 export default API_BASE_URL;
